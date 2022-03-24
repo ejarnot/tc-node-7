cf96ccee1718eb500afb895d56bc24e427f567d9
Ben Bryant <brbryant2639@gmail.com>
2022-03-24 10:30:54 -0500

    UPDATE: operators review

--- exercises/js/control-flow.js
+++ exercises/js/control-flow.js
@@ -1,42 +1,14 @@
-let numToGuess = 5;
+console.log("Can you guess the result?\n12 >= 10 && '0' || 0\n\n");
 
-let userNum = prompt("Guess a number between 1 and 10.");
+// Order of Precedence for logical operators
+// Short Circuiting, or McCarthy's Evaluation
 
-if (numToGuess > userNum) {
-  alert("Too low");
-} else if (numToGuess < userNum) {
-  alert("Too high");
-} else {
-  alert("Congrats!");
-}
+let resultA = "0" && "a true string"; // returns "a true string"
+let resultB = "a true string" && "0"; // returns "0"
+let resultC = "0" || "a true string"; // returns "0"
+let resultD = "" || "a true string"; // returns "a true string"
 
-let userBirthMonth = prompt(
-  "What month were you born?\nType in the abbreviated month (ex: January -> Jan):"
-);
-
-// abbreviated, tell them the season
-
-switch (userBirthMonth) {
-  case "Dec":
-  case "Jan":
-  case "Feb":
-  case "Mar":
-    alert("You were born in winter");
-    break;
-  case "Apr":
-  case "May":
-  case "Jun":
-    alert("You were born in spring");
-    break;
-  case "Jul":
-  case "Aug":
-    alert("You were born in summer");
-    break;
-  case "Sept":
-  case "Oct":
-  case "Nov":
-    alert("You were born in fall");
-    break;
-  default:
-    alert("You gave me an incorrect month format.");
-}
+console.log(resultA);
+console.log(resultB);
+console.log(resultC);
+console.log(resultD);
