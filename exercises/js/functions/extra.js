f70ea8352089ed1fdfbc27ae43a6f78cf3e16cf4
Ben Bryant <brbryant2639@gmail.com>
2022-03-25 07:58:20 -0500

    UPDATE: added draft for extra functions practice

---
+++ exercises/js/functions/extra.js
@@ -0,0 +1,47 @@
+/*
+Write an app to help people track their cell phone usage. Cell phone
+plans for this particular company give you a certain number of minutes every 30 days
+which must be used or they are lost (no rollover). We want to track the average number
+of minutes used per day and inform the user if they are using too many minutes or can
+afford to use more.
+
+Write a program that prompts the user to enter the following pieces of data:
+
+• Number of minutes in the plan per 30 day period, m
+• The current day in the 30 day period, d
+• The total number of minutes used so far u
+
+The program should then compute whether the user is over, under, or right on the average
+daily usage under the plan. It should also inform them of how many minutes are left
+and how many, on average, they can use per day for the rest of the month. Of course, if
+they’ve run out of minutes, it should inform them of that too.
+
+For example, if the user enters m = 250, d = 10, and u = 150, your program should print
+out something similar to the following.
+
+10 days used, 20 days remaining
+Average daily use: 15 min/day
+You are EXCEEDING your average daily use (8.33 min/day),
+continuing this high usage, you'll exceed your minute plan by
+200 minutes.
+To stay below your minute plan, use no more than 5 min/day.
+*/
+
+/*
+Write a program that prompts the user for an x and a y coordinate in
+the Cartesian plane and prints out a message indicating if the point (x, y) lies on an axis
+(x or y axis, or both) or what quadrant it lies in
+*/
+
+/*
+A triangle can be characterized in terms of the length of its three sides.
+In particular, an equilateral triangle is a triangle with all three sides being equal. A
+triangle such that two sides have the same length is isosceles and a triangle with all three
+sides having a different length is scalene. Examples of each can be found in Figure 3.5.
+In addition, the three sides of a triangle are valid only if the sum of any two sides is
+strictly greater than the third length.
+
+Write a program to read in three numbers as the three sides of a triangle. If the three
+sides do not form a valid triangle, you should indicate so. Otherwise, if valid, your
+program should output whether or not the triangle is equilateral, isosceles or scalene.
+*/
