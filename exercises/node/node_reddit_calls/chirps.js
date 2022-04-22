a960784f0724d4077a8085516af62aa8a896381d
Ben <brbryant2639@gmail.com>
2022-04-22 15:04:45 -0500

    UPDATE: added node reddit calls lab parts 1 and 2

---
+++ exercises/node/node_reddit_calls/chirps.js
@@ -0,0 +1,35 @@
+const { writeFile } = require("fs");
+const { join } = require("path");
+
+const filePath = join(__dirname, "./chirps.json");
+
+const contents = [
+  {
+    author: "Ben",
+    content: "First chirp",
+  },
+  {
+    author: "Seth",
+    content: "Hello World",
+  },
+  {
+    author: "Ben",
+    content: "Node is cool",
+  },
+  {
+    author: "Michael",
+    content: "C# is chirp",
+  },
+  {
+    author: "Cruz",
+    content: "Rocket league is coollll",
+  },
+];
+
+writeFile(filePath, JSON.stringify(contents), (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Successfully wrote chirps to file.");
+  }
+});
